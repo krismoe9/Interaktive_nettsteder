f790d9301524a0e89473ac74b9f1c3457b9c5a91
krismoe9 <krismoe@hiof.no>
2026-02-09 16:46:21 +0100

    Arbeidskrav 2

--- Arbeidskrav_2/src/App.jsx
+++ Arbeidskrav_2/src/App.jsx
@@ -2,28 +2,14 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  // Her legger du inn forhåndsdefinerte varer
-  const [items, setItems] = useState([
-    { id: 1, name: 'Melk', quantity: 2, bought: true },
-    { id: 2, name: 'Brød', quantity: 1, bought: false }
-  ]);
-
+  const shoppingList = [
+    { id: 1, name: "Brød", quantity: 1, purchased: false },
+    { id: 2, name: "Egg", quantity: 12, purchased: true },
+    { id: 3, name: "Ost", quantity: 1, purchased: false }
+  ];
+  
   return (
-    <main>
-      <h1>Handleliste</h1>
 
-      {/*Ul-liste som viser alle varer */}
-      <ul>
-        {items.map(item => {
-          //Lag ett <li> per vare
-          return (
-            <li key={item.id}>
-              {item.name} - {item.quantity} - {item.bought ? 'Kjøpt' : 'Ikke kjøpt'}
-            </li>
-          )
-        })}
-      </ul>
-    </main>
   );
 }
 
