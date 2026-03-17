a24e86b0a91014507827c09e61716a55121d9cb1
krismoe9 <krismoe@hiof.no>
2026-03-17 10:08:09 +0100

    Arbeidskrav 2.1

---
+++ A_2.1/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+
+function App() {
+
+ const [shoppingList, setShoppingList] =useState()
+
+  return (
+    <section>
+    <h1>Handleliste</h1>
+    </section>
+  )
+
+
+}
+
+export default App
